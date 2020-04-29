f8382dcfebbc62dc885b9914a398a791c724daf7
Sachin Duhan <duhan.sachin2610@gmail.com>
2020-04-30 04:12:30 +0530

    killing CTRL + S key on website

--- js/theme.js
+++ js/theme.js
@@ -35,29 +35,15 @@
         slide();
     }, 5000);
 
-    /**
-     * --------------------------------------------------------
-     * submitted the form!
-     * --------------------------------------------------------
-     */
-    let testForm = document.querySelector("#contactForm");
+    function noSavingAllowed(params) {
+        $(document).bind('keydown', function(e) {
+            if (e.ctrlKey && (e.which == 83)) {
+                e.preventDefault();
+                // alert('Ctrl+S');
+                return false;
+            }
+        });
+    }
+    noSavingAllowed();
 
-    testForm.addEventListener('submit', e => {
-        e.preventDefault();
-
-        const formData = new FormData(testForm);
-        fetch(testForm.getAttribute('action'), {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
-                    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-                },
-                body: new URLSearchParams(formData).toString()
-            })
-            .then(res => {
-                if (res) {
-                    alert('Thank for your submission!');
-                }
-            });
-    });
 })(jQuery);
